50d1e6c55240a62048e10a0e8053097afad657e3
Vladivir <v.ambrazhey@lovata.com>
2019-04-07 21:10:07 +0300

    Add copyright

--- src/oc-request-message.js
+++ src/oc-request-message.js
@@ -1,3 +1,7 @@
+/**
+ * @author  Uladzimir Ambrazhey, <uladzimir.ambrazhey@gmail.com>
+ */
+
 export default {
   handlerUndefined: 'The request handler name is not specified.',
   HandlerNameInvalid: 'Invalid handler name. The correct handler name format is: "onEvent".',
