76a77c420e26dc0f0d54d86035ad13a03b28b970
Vladivir <v.ambrazhey@lovata.com>
2019-04-07 21:10:17 +0300

    Add update dom class

---
+++ src/oc-update-dom.js
@@ -0,0 +1,50 @@
+export default class OCUpdateDOM {
+  constructor(data = {}, response = {}) {
+    this.response = response;
+    this.data = data;
+  }
+
+  update() {
+    this.parseDataObject(this.data);
+  }
+
+  parseDataObject(data) {
+    [...Object.keys(data)].forEach((partial) => {
+      const selector = data[partial];
+
+      if (typeof selector !== 'string') return;
+
+      this.selectSelectorHandler(partial, selector);
+    });
+  }
+
+  selectSelectorHandler(partial, selector) {
+    switch (selector.charAt(0)) {
+      case '@': this.smartInsert('beforeEnd', partial, selector);
+        break;
+
+      case '^': this.smartInsert('afterBegin', partial, selector);
+        break;
+
+      default: this.replace(partial, selector);
+    }
+  }
+
+  replace(partial, selector) {
+    [...document.querySelectorAll(selector)].forEach((el) => {
+      const html = this.response.data[partial];
+      const node = el;
+
+      node.innerHTML = html;
+    });
+  }
+
+  smartInsert(where, partial, selector) {
+    [...document.querySelectorAll(selector.substring(1))].forEach((el) => {
+      const html = this.response.data[partial];
+      const node = el;
+
+      node.insertAdjacentHTML(where, html);
+    });
+  }
+}
