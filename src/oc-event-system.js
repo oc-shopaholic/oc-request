cdd22ab467e8133cf7399477f7a8c513795b23b2
v.ambrazhei <v.ambrazhey@lovata.com>
2019-05-19 18:05:17 +0300

    Add new events

--- src/oc-event-system.js
+++ src/oc-event-system.js
@@ -3,6 +3,11 @@ export default new class OCEventSystem {
     this.ocBeforeRequestEvent = 'ocBeforeAxiosRequest';
     this.ocBeforeUpdateEvent = 'ocBeforeUpdateEvent';
     this.ocAfterUpdateEvent = 'ocAfterUpdateEvent';
+    this.OCBeforeSuccessEvent = 'ocBeforeSuccessEvent';
+    this.OCAfterSuccessEvent = 'ocAfterSuccessEvent';
+    this.OCBeforeCompleteEvent = 'ocBeforeCompleteEvent';
+    this.OCAfterCompleteEvent = 'ocAfterCompleteEvent';
+    this.OCBeforeRedirectEvent = 'ocBeforeRedirectEvent';
   }
 
   ocBeforeRequest(detail) {
@@ -32,5 +37,55 @@ export default new class OCEventSystem {
     });
 
     return ocAfterUpdate;
+  }
+
+  OCBeforeSuccessEvent(data) {
+    const OCBeforeSuccessEvent = new CustomEvent(this.OCBeforeSuccessEvent, {
+      bubbles: true,
+      cancelable: false,
+      details: data,
+    });
+
+    return OCBeforeSuccessEvent;
+  }
+
+  OCAfterSuccessEvent(data) {
+    const OCAfterSuccessEvent = new CustomEvent(this.OCAfterSuccessEvent, {
+      bubbles: true,
+      cancelable: false,
+      details: data,
+    });
+
+    return OCAfterSuccessEvent;
+  }
+
+  OCBeforeCompleteEvent(data) {
+    const OCBeforeCompleteEvent = new CustomEvent(this.OCBeforeCompleteEvent, {
+      bubbles: true,
+      cancelable: false,
+      details: data,
+    });
+
+    return OCBeforeCompleteEvent;
+  }
+
+  OCAfterCompleteEvent(data) {
+    const OCAfterCompleteEvent = new CustomEvent(this.OCAfterCompleteEvent, {
+      bubbles: true,
+      cancelable: false,
+      details: data,
+    });
+
+    return OCAfterCompleteEvent;
+  }
+
+  OCBeforeRedirectEvent(data) {
+    const OCBeforeRedirectEvent = new CustomEvent(this.OCBeforeRedirectEvent, {
+      bubbles: true,
+      cancelable: false,
+      details: data,
+    });
+
+    return OCBeforeRedirectEvent;
   }
 }();
